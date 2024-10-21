d8f87aa0a9b2f372a322fe8d489a541394550312
lucasgiorgi95 <lucas1995giorgi@gmail.com>
2024-10-21 01:11:15 -0300

    fix errors

--- pages/page-home.php
+++ pages/page-home.php
@@ -9,24 +9,21 @@ Template Name: Home
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Terapia psicológica y espiritual: sana mente, cuerpo y espíritu a través de Constelaciones familiares, canalización con guías y seres espirituales; limpieza energética o defensa psíquica. Sesiones presenciales y en línea.">
-    <meta name="keywords" content="terapia, psicológica,presencial, en línea, psicóloga, psicoterapeuta, Nuevo, León">
+    <meta name="description" content="Organizamos experiencias únicas y memorables: eventos que conectan como convenciones, lanzamientos, fiestas corporativas y más. ¡Contáctanos!">
+    <meta name="keywords" content="Agencia de publicidad creativa, Marketing digital, Estrategia de branding, Diseño gráfico, Creatividad, Publicidad en redes sociales, Agencia de marketing digital, Desarrollo de contenido, Estrategia de contenido, Campañas publicitarias, Publicidad digital, Estrategias creativas, Creación de marca, Creatividad e innovación, Agencia de diseño digital, Paid Media, Performance, Estrategia de comunicación">
 
-    <meta property="og:title" content="Cosnthera">
-    <meta property="og:description"
-        content="Descubre un espacio de sanación. Nuestros servicios incluyen: terapia psicológica, constelaciones familiares, limpieza energética, defensa psíquica y canalización espiritual. Atención online y presencial.">
+    <meta property="og:title" content="Punto de Encuentro: Experiencias únicas para cada evento">
+    <meta property="og:description" content="Transformamos ideas en emociones: somos una agencia creativa que desarrolla soluciones de marketing efectivas para lograr tus metas.">
     <meta property="og:type" content="website">
     <meta property="og:url" content="<?php echo get_permalink(); ?>">
 
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Cosnthera">
-    <meta name="twitter:description"
-        content="Espacio psicoterapéutico y holístico en donde te reconciliarás con el pasado y el presente para equilibrar tu futuro. Terapia presencial y en línea.">
+    <meta name="twitter:description" content="Espacio psicoterapéutico y holístico en donde te reconciliarás con el pasado y el presente para equilibrar tu futuro. Terapia presencial y en línea.">
 
     <link rel="canonical" href="<?php echo get_permalink(); ?>">
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-36VC4346Y2"></script>
+    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-36VC4346Y2"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
 
@@ -35,16 +32,15 @@ Template Name: Home
     }
     gtag('js', new Date());
     gtag('config', 'G-36VC4346Y2');
-    </script>
+    </script> -->
 
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/tailwind_output.css">
     <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/formStyles.css">
-    <link rel="stylesheet"
-        href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/node_modules/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/node_modules/swiper/swiper-bundle.min.css">
 
     <title>Pachuco</title>
-    <link rel="icon" href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/Assets/Landing/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="<?php echo get_site_url(); ?>/wp-content/themes/Pachuco/Assetss/Landing/favicon.ico" type="image/x-icon" />
     
     <?php wp_head(); ?>
 </head>
