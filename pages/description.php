ea0445615e66f3b5c821565b9226ac28e3034e1f
Hector Amador Hernandez <hecttor9@gmail.com>
2024-11-21 21:48:37 -0600

    fix description

--- pages/description.php
+++ pages/description.php
@@ -1,5 +1,5 @@
 <section class="px-5 bg-[white] py-4 lg:mx-auto">
-  <div class="flex flex-col lg:max-w-[1200px] mx-auto items-center justify-center text-center">
+  <div class="flex flex-col lg:max-w-[1090px] mx-auto items-center justify-center text-center">
     <h2 class="text-black font-Montse text-[0.875rem] md:text-[1rem] mt-[1rem] md:mt-[1.5rem]">
       En <strong>Punto de Encuentro</strong> by Pachuco,somos apasionados en la organización de eventos presenciales, virtuales e híbridos.
       Transformamos tus ideas en experiencias que dejan huella, logrando una conexión significativa de las marcas con sus audiencias.
